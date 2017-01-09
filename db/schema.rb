dc46d0ef1eb1103fb198008903415315c50d1d66
yourUsername <your_email@example.com>
2017-01-09 05:42:28 -0500

    Updated movie form

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109045426) do
+ActiveRecord::Schema.define(version: 20170109103920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "title"
+    t.text     "description"
+    t.string   "director"
   end
 
   create_table "reviews", force: :cascade do |t|
